b281320d01d81dd7722f4b10ce42d3f01808d7db
Tazthor <sprat@omnium.cat>
2024-03-26 09:43:13 +0100

    change name variables env

--- src/App.tsx
+++ src/App.tsx
@@ -18,14 +18,14 @@ import "typeface-rubik";
 import "@fontsource/ibm-plex-mono";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_databaseURL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_measurementId,
+  apiKey: process.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VITE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VITE_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
